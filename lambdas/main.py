b0fb7b79a690100e557b1f5766c9dde22e3fd158
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2024-11-23 19:14:37 +0100

    add db connection and Peep model

--- lambdas/main.py
+++ lambdas/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from .routes import peeps, users
 
+from .routes import peeps, users
 
 app = FastAPI()
 
@@ -8,6 +8,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "this is root!"}
+
 
 app.include_router(peeps.router)
 app.include_router(users.router)
