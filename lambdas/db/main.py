3e65c656d97ad154fad134c7acc1fdcea0bc966b
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2024-11-23 20:17:22 +0100

    add crud to users

--- lambdas/db/main.py
+++ lambdas/db/main.py
@@ -29,6 +29,17 @@ class Peep(Base):
     updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False, onupdate=func.now())
 
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, server_default=func.uuid_generate_v4())
+    name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), nullable=False, onupdate=func.now())
+
+
 def get_db_session():
     """
     Dependency or utility function to provide a database session.
