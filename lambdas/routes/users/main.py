26eb8e852bfce870e4438f1297b3fc030a27ff38
Leonardo Guedez <leonardoguedezsuazo@gmail.com>
2024-12-06 12:52:15 +0100

    add crude timeline feature

--- lambdas/routes/users/main.py
+++ lambdas/routes/users/main.py
@@ -3,11 +3,13 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from sqlalchemy import desc, select
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
 
 from lambdas.routes.authentication.main import router as authentication_router
 from ..authentication.utils import check_logged_in
-from ...db import Follows, User, get_db_session
+from ...db import Follows, Peep, User, get_db_session
 from ...dtos.users import FollowRequestDTO, UpdateRequestDTO
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -78,6 +80,27 @@ async def unfollow(user_id: UUID, who: FollowRequestDTO, db: Session = Depends(g
     db.delete(follows)
     db.commit()
     return {'message': 'User unfollowed successfully'}
+
+
+@router.get('/{user_id}/timeline')
+async def fetch_timeline(user_id: str, db: Session = Depends(get_db_session),
+                         is_logged_in: bool = Depends(check_logged_in)):
+    if not is_logged_in:
+        raise HTTPException(status_code=401, detail='Not logged in', headers={'WWW-Authenticate': 'Bearer'})
+
+    # TODO: find a way to improve this
+    query = (
+        select(Peep.content,
+               func.to_char(Peep.created_at, 'YYYY-MM-DD"T"HH24:MI "UTC"OF').label('created_at'))
+        .join(Follows, Follows.followee_id == Peep.user_id)  # Join peeps on follows
+        .join(User, User.id == Follows.followee_id)  # Join users on follows
+        .filter(Follows.follower_id == user_id)
+        .order_by(desc(Peep.created_at))
+    )
+
+    timeline = db.execute(query).mappings().all()
+
+    return timeline
 
 
 def extract_set_fields(body: BaseModel):
